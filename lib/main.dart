8e5734e3c399dfde5615ec66e33c301580e827f5
SERGIO_IVAN_GURROLA_CERVANTES <a.20308051280498@cbtis128.edu.mx>
2023-05-12 13:30:41 +0000

    TextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,22 +9,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
       title: 'Flutter Hello World',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Jmas'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
@@ -32,10 +34,76 @@ class MyHomePage extends StatelessWidget {
         // The title text which will be shown on the action bar
         title: Text(title),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: ListView(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: Text(
+              'Ingresar sus datos',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                height: 1.5,
+              ),
+            ),
+          ),
+          Container(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "N0mbre",
+                    labelText: "Nombre",
+                    prefixIcon: Icon(Icons.person)),
+              )),
+          Container(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Numero",
+                    labelText: "Numero",
+                    prefixIcon: Icon(Icons.numbers)),
+              )),
+          Container(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Correo",
+                    labelText: "Correo",
+                    prefixIcon: Icon(Icons.email)),
+              )),
+          Container(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Contraseña",
+                    labelText: "Contraseña",
+                    prefixIcon: Icon(Icons.password)),
+              )),
+          Container(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Contraseña",
+                    labelText: "Confirmar Contraseña",
+                    prefixIcon: Icon(Icons.password)),
+              )),
+          Container(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Ubicacion",
+                    labelText: "Ubicacion",
+                    prefixIcon: Icon(Icons.place_outlined)),
+              )),
+          Container(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "usuario",
+                    labelText: "usuario",
+                    prefixIcon: Icon(Icons.person)),
+              ))
+        ],
       ),
     );
   }
